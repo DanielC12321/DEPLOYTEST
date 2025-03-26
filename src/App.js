4da94c61181e82caf6a19fa50c4fca33bf028d1a
ValMobile <daniel.chn0303@gmail.com>
2025-03-25 21:13:46 -0500

    Add progress

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
+// App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Cashier from './pages/cashier';
 import Customer from './pages/customer';
-import Manager from './pages/manager';
+// Import the manager interface component (update the path if needed)
+import ManagerInterface from './pages/ManagerInterface.tsx';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/cashier">Cashier</Link>
-            </li>
-            <li>
-              <Link to="/customer">Customer</Link>
-            </li>
-            <li>
-              <Link to="/manager">Manager</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/cashier" element={<Cashier />} />
-          <Route path="/customer" element={<Customer />} />
-          <Route path="/manager" element={<Manager />} />
-        </Routes>
-      </div>
-    </Router>
+      <Router>
+        <div className="App">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/cashier">Cashier</Link>
+              </li>
+              <li>
+                <Link to="/customer">Customer</Link>
+              </li>
+              <li>
+                <Link to="/manager">Manager</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/cashier" element={<Cashier />} />
+            <Route path="/customer" element={<Customer />} />
+            <Route path="/manager" element={<ManagerInterface />} />
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
---
+++ src/pages/ManagerInterface.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import ProductUsageChart from './ProductUsageChart.tsx';
+
+interface ManagerInterfaceState {
+    selectedReport: string;
+}
+
+class ManagerInterface extends React.Component<{}, ManagerInterfaceState> {
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            selectedReport: '',
+        };
+    }
+
+    handleReportClick = (reportName: string) => {
+        this.setState({ selectedReport: reportName });
+    };
+
+    renderReport() {
+        const { selectedReport } = this.state;
+        switch (selectedReport) {
+            case 'productUsage':
+                // Make sure the ProductUsageChart component is imported correctly.
+                return <ProductUsageChart />;
+            case 'salesReport':
+                return <div>Sales Report Content (To Be Implemented)</div>;
+            case 'xReport':
+                return <div>X Report Content (To Be Implemented)</div>;
+            case 'zReport':
+                return <div>Z Report Content (To Be Implemented)</div>;
+            default:
+                return <div>Please select a report to view.</div>;
+        }
+    }
+
+    render() {
+        return (
+            <div style={{ padding: '20px' }}>
+                <h1>Manager Interface</h1>
+                <div style={{ marginBottom: '20px' }}>
+                    <button onClick={() => this.handleReportClick('productUsage')}>
+                        Product Usage Chart
+                    </button>
+                    <button onClick={() => this.handleReportClick('salesReport')}>
+                        Sales Report
+                    </button>
+                    <button onClick={() => this.handleReportClick('xReport')}>
+                        X Report
+                    </button>
+                    <button onClick={() => this.handleReportClick('zReport')}>
+                        Z Report
+                    </button>
+                </div>
+                <div>{this.renderReport()}</div>
+            </div>
+        );
+    }
+}
+
+export default ManagerInterface;
