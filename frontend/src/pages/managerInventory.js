d17fdad1eca96296feb679c6398ac7f4a7006f94
norve11z <norvezac000@tamu.edu>
2025-03-28 00:04:58 -0500

    Added Manager Inventory Page

---
+++ frontend/src/pages/managerInventory.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./managerInventory.css";
+
+
+
+function ManagerInventory() {
+  
+  const navigate = useNavigate();
+
+  const toManager = () => {
+    navigate("/ManagerInterface");
+  }
+
+  return (
+        <div id="wrapper">
+        <div class="divs" id="div1"><button onClick={toManager} id="back">Manager Home</button></div>
+        <div class="divs" id="div2"><h1 id="header">Inventory</h1></div>
+        <div class="divs" id="div3">Div</div>
+        <div class="divs" id="div4">Div</div>
+        <div class="divs" id="div5">Div</div>
+        <div class="divs" id="div6">Div</div>
+        <div class="divs" id="div7">Div</div>
+        <div class="divs" id="div8">Div</div>
+        <div class="divs" id="div9">Div</div>
+        </div>
+  );
+}
+
+export default ManagerInventory;
