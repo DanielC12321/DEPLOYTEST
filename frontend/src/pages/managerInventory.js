02e48819c78aa678c2ba83137b3a7ee24283d8a0
norve11z <norvezac000@tamu.edu>
2025-03-30 00:52:25 -0500

    Added Inventory Tables

--- frontend/src/pages/managerInventory.js
+++ frontend/src/pages/managerInventory.js
@@ -12,17 +12,89 @@ function ManagerInventory() {
     navigate("/ManagerInterface");
   }
 
+  const data = [
+    { product: 'Milk', price: 1, quantity: -1 },
+    { product: 'Tea', price: 2, quantity: -2 },
+    { product: 'Juice', price: 3, quantity: -3 },
+    { product: 'Milk', price: 1, quantity: -1 },
+    { product: 'Tea', price: 2, quantity: -2 },
+    { product: 'Juice', price: 3, quantity: -3 },    
+    { product: 'Milk', price: 1, quantity: -1 },
+    { product: 'Tea', price: 2, quantity: -2 },
+    { product: 'Juice', price: 3, quantity: -3 },   
+    { product: 'Milk', price: 1, quantity: -1 },
+    { product: 'Tea', price: 2, quantity: -2 },
+    { product: 'Juice', price: 3, quantity: -3 },  
+    { product: 'Milk', price: 1, quantity: -1 },
+    { product: 'Tea', price: 2, quantity: -2 },
+    { product: 'Juice', price: 3, quantity: -3 },   
+    { product: 'Milk', price: 1, quantity: -1 },
+    { product: 'Tea', price: 2, quantity: -2 },
+    { product: 'Juice', price: 3, quantity: -3 },  
+    { product: 'Milk', price: 1, quantity: -1 },
+    { product: 'Tea', price: 2, quantity: -2 },
+    { product: 'Juice', price: 3, quantity: -3 },
+    { product: 'Milk', price: 1, quantity: -1 },
+    { product: 'Tea', price: 2, quantity: -2 },
+    { product: 'Juice', price: 3, quantity: -3 },   
+    { product: 'Milk', price: 1, quantity: -1 },
+    { product: 'Tea', price: 2, quantity: -2 },
+    { product: 'Juice', price: 3, quantity: -3 },   
+    { product: 'Milk', price: 1, quantity: -1 },
+    { product: 'Tea', price: 2, quantity: -2 },
+    { product: 'Juice', price: 3, quantity: -3 },    
+    { product: 'Milk', price: 1, quantity: -1 },
+    { product: 'Tea', price: 2, quantity: -2 },
+    { product: 'Juice', price: 3, quantity: -3 },   
+    { product: 'Milk', price: 1, quantity: -1 },
+    { product: 'Tea', price: 2, quantity: -2 },
+    { product: 'Juice', price: 3, quantity: -3 },
+];
+
+const CreateTable = () => {
+
+  return(
+      <table class="tables">
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((entry, i) => (
+            <tr key={i}>
+              <td>{entry.product}</td>
+              <td>{entry.price}</td>
+              <td>{entry.quantity}</td>
+            </tr>
+          ))
+          }
+        </tbody>
+      </table>
+
+  );
+};
+
   return (
         <div id="wrapper">
         <div class="divs" id="div1"><button onClick={toManager} id="back">Manager Home</button></div>
         <div class="divs" id="div2"><h1 id="header">Inventory</h1></div>
         <div class="divs" id="div3">Div</div>
         <div class="divs" id="div4">Div</div>
-        <div class="divs" id="div5">Div</div>
+        <div class="divs" id="div5">
+          <div class="TableDivs">
+            <div class="scrolltable"><h2>Products</h2>
+              <CreateTable />
+            </div>
+            <div class="scrolltable"><h2>Ingredients</h2>
+              <CreateTable />
+            </div>
+         </div>
+        </div>
         <div class="divs" id="div6">Div</div>
-        <div class="divs" id="div7">Div</div>
-        <div class="divs" id="div8">Div</div>
-        <div class="divs" id="div9">Div</div>
+
         </div>
   );
 }
