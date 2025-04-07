b2e2081f460f8a34d53f50f76a80f690df34147c
xxilytoo <contactkenhao@gmail.com>
2025-04-07 15:37:40 -0500

    working backend api

--- backend/routes/gemmaChatbot.js
+++ backend/routes/gemmaChatbot.js
@@ -4,14 +4,30 @@ require("dotenv").config();
 
 const router = express.Router();
 
-// Google API Configuration
-const genomics = google.genomics("v2alpha1");
-const serviceAccountKey = JSON.parse(process.env.GOOGLE_GEMMA_KEY); // Load your service account key from environment variable
-const auth = new google.auth.GoogleAuth({
-  credentials: serviceAccountKey,
-  scopes: ["https://www.googleapis.com/auth/cloud-platform"],
-});
+const googleGenerativeAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_API_KEY);
+const gemini = googleGenerativeAI.getGenerativeModel({model: 'gemini-2.0-flash'})
 
+router.post("/stream", async (req, res) => {
+  try {
+      /** Read the request data. */
+      const chatHistory = req.body.history || [];
+      const msg = req.body.chat;
+
+      const chat = gemini.startChat({
+          history: chatHistory,
+      });
+
+      const result = await chat.sendMessageStream(msg);
+      for await (const chunk of result.stream) {
+          const chunkText = chunk.text();
+          res.write(chunkText);
+      }
+      res.end();
+  } catch (error) {
+      console.error("Error in /stream route:", error);
+      res.status(500).json({ error: "Failed to process the chat request." });
+  }
+});
 
 
 module.exports = router;
